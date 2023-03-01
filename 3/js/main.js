85c4dc556a87fa5c15d43a58f31907ca53c32672
Keks <keks@htmlacademy.ru>
2023-03-01 14:35:20 +0300

    :heavy_check_mark: Сборка #3

--- 3/js/main.js
+++ 3/js/main.js
@@ -1,3 +1,17 @@
+const MIN_COUNT_USERS_ID = 1;
+const MAX_COUNT_USERS_ID = 25;
+const MIN_COUNT_COMMENTS_ID = 26;
+const MAX_COUNT_COMMENTS_ID = 1000;
+const MIN_COUNT_PHOTOS_ID = 1;
+const MAX_COUNT_PHOTOS_ID = 25;
+const MIN_COUNT_LIKE = 15;
+const MAX_COUNT_LIKE = 200;
+const MIN_COUNT_AVATAR = 1;
+const MAX_COUNT_AVATAR = 6;
+
+
+const SIMILAR_USERS_COUNT = 25;
+
 const NAMES = [
   'Иван',
   'Андрей',
@@ -26,7 +40,7 @@ const NAMES = [
   'Таня',
 ];
 
-const DESCRIPTION = [
+const DESCRIPTIONS = [
   'Какой хороший день!',
   'А ведь так все здорово',
   'Этот день был великолепен',
@@ -36,7 +50,7 @@ const DESCRIPTION = [
   'Так начинается мое типичное утро',
 ];
 
-const MESSAGE = [
+const MESSAGES = [
   'Всё отлично!',
   'В целом всё неплохо. Но не всё.',
   'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
@@ -46,8 +60,10 @@ const MESSAGE = [
 ];
 
 function getRandomInteger (min, max) {
-  const lower = Math.ceil(Math.min(Math.abs(min), Math.abs(max)));
-  const upper = Math.floor(Math.max(Math.abs(min), Math.abs(max)));
+  const minValue = Math.abs(min);
+  const maxValue = Math.abs(max);
+  const lower = Math.ceil(Math.min(minValue, maxValue));
+  const upper = Math.floor(Math.max(minValue, maxValue));
   const result = Math.random() * (upper - lower + 1) + lower;
 
   return Math.floor(result);
@@ -58,10 +74,6 @@ function createRandomIdFromRangeGenerator (min, max) {
 
   return function () {
     let currentValue = getRandomInteger(min, max);
-
-    if (previousValues.length >= (max - min + 1)) {
-      return null;
-    }
 
     while (previousValues.includes(currentValue)) {
       currentValue = getRandomInteger(min, max);
@@ -75,24 +87,24 @@ function createRandomIdFromRangeGenerator (min, max) {
 
 const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
 
-const getUniqueCommentId = createRandomIdFromRangeGenerator(26, 1000);
-const getUniqueUserId = createRandomIdFromRangeGenerator(1, 25);
-const getUniquePhotoId = createRandomIdFromRangeGenerator(1, 25);
+const getUniqueCommentId = createRandomIdFromRangeGenerator(MIN_COUNT_COMMENTS_ID, MAX_COUNT_COMMENTS_ID);
+const getUniqueUserId = createRandomIdFromRangeGenerator(MIN_COUNT_USERS_ID, MAX_COUNT_USERS_ID);
+const getUniquePhotoId = createRandomIdFromRangeGenerator(MIN_COUNT_PHOTOS_ID, MAX_COUNT_PHOTOS_ID);
 
 const createUserComments = () => ({
   id: getUniqueCommentId(),
-  avatar: `img/avatar-${ getRandomInteger(1, 6) }.svg`,
-  message: getRandomArrayElement(MESSAGE),
+  avatar: `img/avatar-${ getRandomInteger(MIN_COUNT_AVATAR, MAX_COUNT_AVATAR) }.svg`,
+  message: getRandomArrayElement(MESSAGES),
   name: getRandomArrayElement(NAMES),
 });
 
 const createUser = () => ({
   id: getUniqueUserId(),
   url: `photos/${ getUniquePhotoId() }.jpg`,
-  description: getRandomArrayElement(DESCRIPTION),
-  likes: getRandomInteger(15, 200),
+  description: getRandomArrayElement(DESCRIPTIONS),
+  likes: getRandomInteger(MIN_COUNT_LIKE, MAX_COUNT_LIKE),
   name: getRandomArrayElement(NAMES),
   comment: createUserComments(),
 });
 
-Array.from({length: 25}, createUser);
+Array.from({length: SIMILAR_USERS_COUNT}, createUser);

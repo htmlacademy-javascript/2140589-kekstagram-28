2acafc08f41b0db4b965deb21d3a5f6026080ca1
Andrey <kaliushka890@gmail.com>
2023-03-23 23:07:56 +0700

    add rendering big picture

--- js/user-comments.js
+++ js/user-comments.js
@@ -1,10 +1,13 @@
 const commentsList = document.querySelector('.social__comments');
 const socialComment = document.querySelector('.social__comment');
 const commentsListFragment = document.createDocumentFragment();
+let commentsShown = 0;
+const extraComments = 5;
+const commentsCounter = document.querySelector('.social__comment-count');
+const commentsLoader = document.querySelector('.comments-loader');
+let savedComments = [];
 
 const createTempletComments = function (comments) {
-  commentsList.innerHTML = '';
-
   comments.forEach(({avatar, message, name}) => {
     const userComment = socialComment.cloneNode(true);
 
@@ -18,4 +21,29 @@ const createTempletComments = function (comments) {
   commentsList.appendChild(commentsListFragment);
 };
 
-export {createTempletComments};
+const showComments = function (comments) {
+  savedComments = comments;
+
+  if (comments.length > commentsShown + extraComments) {
+    commentsLoader.classList.remove('hidden');
+  } else {
+    commentsLoader.classList.add('hidden');
+  }
+
+  createTempletComments(comments.slice(commentsShown, extraComments + commentsShown));
+
+  commentsCounter.innerHTML = `${comments.length < commentsShown + extraComments ? comments.length : commentsShown + extraComments} из <span class="comments-count">${comments.length}</span> комментариев`;
+};
+
+const clickOnLoader = function () {
+  commentsShown += extraComments;
+  showComments(savedComments);
+};
+
+const resetCommentsShown = function() {
+  commentsShown = 0;
+};
+
+commentsLoader.addEventListener('click', clickOnLoader);
+
+export {showComments, resetCommentsShown};

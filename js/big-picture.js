ad604b65861a68e011d6c9b432a0a531e2382758
Andrey <kaliushka890@gmail.com>
2023-03-24 22:19:25 +0700

    Добавляет мое описание/понимание  функций/кода

--- js/big-picture.js
+++ js/big-picture.js
@@ -3,8 +3,11 @@ import { showComments, resetCommentsShown } from './user-comments.js';
 
 const bigPicture = document.querySelector('.big-picture');
 
+//Эта функция принимает значения массива и записывеает эти значения в элементы.
 const fillBigPicture = ({url, description, likes}) => {
+  //заполняет значением массива путь к картинке ( в src).
   bigPicture.querySelector('.big-picture__img').querySelector('img').src = url;
+  //Заполняет значением массива текстовое содержание массива.
   bigPicture.querySelector('.social__caption').textContent = description;
   bigPicture.querySelector('.likes-count').textContent = likes;
 };
@@ -13,6 +16,7 @@ const userModalCloseElement = bigPicture.querySelector('.big-picture__cancel');
 
 const body = document.querySelector('body');
 
+// Эта функция позволяет закрывать модальное окно (большую фотографию) на клавищу escape.
 const onDocumentEscapeKeydown = (evt) => {
   if (isEscapeKey(evt)) {
     evt.preventDefault();
@@ -20,20 +24,34 @@ const onDocumentEscapeKeydown = (evt) => {
   }
 };
 
+// Эта функция закрытия модального окна. Она добавляет класс hidden в разметку(закрывает большую картинку),
+//стирает комментарии, убирает скролл с окна.
 function closeUserModal () {
+  // эта функция добавляет класс в разметку.
   bigPicture.classList.add('hidden');
+  // эта функция убирает класс из разметки.
   body.classList.remove('modal-open');
+  // Эта функция обновляет значение счеткича комментариев.
   resetCommentsShown();
 }
 
+//Добавляет обработчик события по клику на закрытие окна.
 userModalCloseElement.addEventListener('click', closeUserModal);
 
+//Эта функция открывает большую картинку, убирая класс hidden, прнимает значения других функций и
+//передает их в функцию которая выводит все на страницу.
 const showBigPicture = (photo) => {
   bigPicture.classList.remove('hidden');
+  //Добавляет скролл.
   body.classList.add('modal-open');
+  // добавляет обработчик собития через функцию клика на клавишу escape.
   document.addEventListener('keydown', onDocumentEscapeKeydown);
+  //эта функция принимает значения массива и заполняет данные элементов.
   fillBigPicture(photo);
+  // Этот код заполняет(в данном случае чистит), элементы разметки (комментраии), добавленные ранее.
   bigPicture.querySelector('.social__comments').innerHTML = '';
+  // Эта функция принимает данные массива из объекта, отрисовывает 5 комментариев на странице,
+  // по клику на "загурзить еще" отрисовывает еще 5.
   showComments(photo.comments);
 };
 
--- js/user-comments.js
+++ js/user-comments.js
@@ -2,48 +2,68 @@ const commentsList = document.querySelector('.social__comments');
 const socialComment = document.querySelector('.social__comment');
 const commentsListFragment = document.createDocumentFragment();
 let commentsShown = 0;
-const extraComments = 5;
+const EXTRA_COMMENTS = 5;
 const commentsCounter = document.querySelector('.social__comment-count');
 const commentsLoader = document.querySelector('.comments-loader');
+//сюда мы выводим  выведенные комментариев из функции showComments.
 let savedComments = [];
 
+// эта функциия прнимает массив комментариев, добавляет полученную информацию в элементы дом дерева,
+// передает полученные комментарии в функцию, которая их отрисовывает.
 const createTempletComments = function (comments) {
+  // этот метод перебирает элементы массива, и позволяет выполнить функцию. В этом случае записать
+  // данные массива в разметку комментариев.
   comments.forEach(({avatar, message, name}) => {
+    // клонирует и возвращает дуплекат элемента.
     const userComment = socialComment.cloneNode(true);
 
     userComment.querySelector('.social__picture').src = avatar;
     userComment.querySelector('.social__picture').alt = name;
     userComment.querySelector('.social__text').textContent = message;
 
+    // заполняет фрагмент новыми данными.
     commentsListFragment.appendChild(userComment);
   });
 
+  // выводит полученный фрагмент в дом-элемент.
   commentsList.appendChild(commentsListFragment);
 };
 
+// Эта функция принимает массив комментариев. Добавляет данные в счетчик. Убирает/добавляет кнопку загурзить еще.
+// отрисовывает комментарии на странице (передает количество отрисованных комментов).
 const showComments = function (comments) {
   savedComments = comments;
 
-  if (comments.length > commentsShown + extraComments) {
+  // сверяет длинну массива и количесвто отрисованных комментариев. Если на странице показанных комментариев меньше, чем длина массива,
+  // то оставляет кнопку загрузки. Если нет, то убирает кнопку.
+  if (comments.length > commentsShown + EXTRA_COMMENTS) {
     commentsLoader.classList.remove('hidden');
   } else {
     commentsLoader.classList.add('hidden');
   }
 
-  createTempletComments(comments.slice(commentsShown, extraComments + commentsShown));
+  //эта функция передает комментарии, а метод возвращает их копию и складывает в зависимости от длины показанных комментариев
+  // до полученной длины после загрузки новых комментариев на каждой итерации.
+  createTempletComments(comments.slice(commentsShown, EXTRA_COMMENTS + commentsShown));
 
-  commentsCounter.innerHTML = `${comments.length < commentsShown + extraComments ? comments.length : commentsShown + extraComments} из <span class="comments-count">${comments.length}</span> комментариев`;
+  // этот код передает значение длины показанных комментариев и длину массива (то есть все комментарии к фотографии).
+  commentsCounter.innerHTML = `${comments.length < commentsShown + EXTRA_COMMENTS ? comments.length : commentsShown + EXTRA_COMMENTS} из <span class="comments-count">${comments.length}</span> комментариев`;
 };
 
+//Эта функция прибавляет 5 комментаривев к текущему колличесвту комментариев на каждой итерации.
 const clickOnLoader = function () {
-  commentsShown += extraComments;
+  // на каждой итерации к количевту показанных комментариев прибавляется 5 комментариев.
+  commentsShown += EXTRA_COMMENTS;
+  // выводит через параметр сохраненные комментарии (5-10-15-20 и тд)
   showComments(savedComments);
 };
 
+// Эта функция обнуляет колиесвто комментариев.
 const resetCommentsShown = function() {
   commentsShown = 0;
 };
 
+//добавляет обработчик событий на кнопку загурзить еще.
 commentsLoader.addEventListener('click', clickOnLoader);
 
 export {showComments, resetCommentsShown};

f47df44b3c5e7de59d63b9568943b88e66a3ec7b
Keks <keks@htmlacademy.ru>
2023-04-04 13:42:51 +0300

    :heavy_check_mark: Сборка #10

---
+++ 10/js/main.js
@@ -0,0 +1,18 @@
+import {renderGallery} from './gallery.js';
+import {clickOnUpload, setUserFormSubmit, closeUserModal } from './form.js';
+import { getData } from './api.js';
+import { showAlert, debounce } from './utils.js';
+import {init, getSortedPictures} from './sorting.js';
+
+getData()
+  .then((newPhotos) => {
+    const debouncedRenderGallery = debounce(renderGallery);
+    init(newPhotos, debouncedRenderGallery);
+    renderGallery(getSortedPictures(newPhotos));
+  }).catch(
+    (err) => showAlert(err.message)
+  );
+
+clickOnUpload();
+
+setUserFormSubmit(closeUserModal);
---
+++ 10/js/photo-templet.js
@@ -0,0 +1,32 @@
+const similarListElement = document.querySelector('.pictures');
+// находит шаблон по id.
+const similarPhotoTemplate = document.querySelector('#picture').content.querySelector('.picture');
+// эта функция, которая клонирует дом дерево.
+const similarListFragment = document.createDocumentFragment();
+
+// Эта функция прнимает значения объекта и передает миниатюры на странице.
+const createTempletPhotos = function (photos, picturesContainer) {
+  //этот код очищает текущие элементы .picture
+  picturesContainer.querySelectorAll('.picture').forEach((element) => element.remove());
+  // Этот метод перебирает каждый элемент масива и позволяет
+  // выполнить функцию (в данном случае записать значения массива в элементы на странице).
+  photos.forEach(({url, likes, comments, description, id}) => {
+    // копирует и возвращает дупликат элемента.
+    const userElement = similarPhotoTemplate.cloneNode(true);
+
+    //заполняет элементы по найденному классу новыми данными из массива.
+    userElement.querySelector('.picture__img').src = url; // заполняет путь к картинке.
+    userElement.querySelector('.picture__img').alt = description; //заполняет скрытое описание фотографии.
+    userElement.querySelector('.picture__likes').textContent = likes; //заполняет полученные данные текстом.
+    userElement.querySelector('.picture__comments').textContent = comments.length; // выводит длину массива комментов.
+    userElement.dataset.userElementId = id;
+
+    //Эта часть кода добавляет во фрагмент полученный резульат записи массива в элементы
+    similarListFragment.appendChild(userElement);
+
+    //Эта чать когда добавляет полученные дочерние эллементы в родительский элемент.
+    similarListElement.appendChild(similarListFragment);
+  });
+};
+
+export {createTempletPhotos};
